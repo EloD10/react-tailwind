93cc66e89083c883b39982a4569df014e88e5806
Eloha <elo210@outlook.fr>
2018-12-02 19:34:28 +0100

    update style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import { Router } from '@reach/router';
-import { I18nProvider } from '@lingui/react';
+import React, { Component } from 'react'
+import { Router } from '@reach/router'
+import { I18nProvider } from '@lingui/react'
 
-import { Home } from './pages';
-import { catalogs } from './config/i18n';
+import { Home } from './pages'
+import { catalogs } from './config/i18n'
 
 class App extends Component {
-    render() {
-        return (
-            <I18nProvider language="en" catalogs={catalogs}>
-                <Router>
-                    <Home path="/" />
-                </Router>
-            </I18nProvider>
-        );
-    }
+  render() {
+    return (
+      <I18nProvider language="en" catalogs={catalogs}>
+        <Router>
+          <Home path="/" />
+        </Router>
+      </I18nProvider>
+    )
+  }
 }
 
-export default App;
+export default App
